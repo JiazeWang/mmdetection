70b360f883d3d3229037668e3aa6a981be6f562e
JiazeWang <jiazewang@csu.edu.cn>
2020-04-13 21:08:35 +0800

    infer

---
+++ infer.py
@@ -0,0 +1,16 @@
+from mmdet.apis import init_detector, inference_detector, show_result
+import mmcv
+
+config_file = 'configs/mask_rcnn_r50_fpn_1x.py'
+checkpoint_file = 'model/mask_rcnn_r50_c4_2x-3cf169a9.pth'
+
+# build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+# test a single image and show the results
+img = 'model/sample/img_00001.jpg'  # or img = mmcv.imread(img), which will only load it once
+result = inference_detector(model, img)
+# visualize the results in a new window
+#show_result(img, result, model.CLASSES)
+# or save the visualization results to image files
+show_result(img, result, model.CLASSES, out_file='model/result_00001.jpg')
